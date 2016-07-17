49a47224a9900be8e9728cb02d7f5d0ead8883ae
Dmitry Starostin <incrop@gmail.com>
2016-07-17 20:50:21 +0300

    Fix header nesting

--- blocks/header/header.bemhtml.js
+++ blocks/header/header.bemhtml.js
@@ -1,11 +1,6 @@
 block('header')(
     tag()('header'),
 
-    content()((_, json) => [
-      json.title && {elem: 'title', content: json.title},
-      json.subtitle && {elem: 'subtitle', content: json.subtitle}
-    ]),
-
     elem('title').tag()('h1'),
 
     elem('subtitle').tag()('p')
