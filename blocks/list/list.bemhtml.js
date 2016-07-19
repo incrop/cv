6b0af70b467954668714016c2c849704fd4d168b
Dmitry Starostin <incrop@gmail.com>
2016-07-20 01:29:32 +0300

    Add education section

--- blocks/list/list.bemhtml.js
+++ blocks/list/list.bemhtml.js
@@ -1,6 +1,11 @@
 "use strict";
 
 block('list')(
+  wrap()((_, json) => [
+    json.title && {elem: 'title', content: json.title},
+    json
+  ]),
+
   tag()('ul'),
 
   content()((_, json) => json.items.map(item => ({
