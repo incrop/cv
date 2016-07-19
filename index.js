2bfdcccc80db6e53f4bbbff4e35fb46c1ca77de3
Dmitry Starostin <incrop@gmail.com>
2016-07-19 19:42:41 +0300

    Remove pobems

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let yamljs = require('yamljs');
 let Make = require('bemaker').Make;
 let bemhtml = require('bem-xjst').bemhtml;
 let postcss = require('postcss');
-let pobems = require('pobems');
 let autoprefixer = require('autoprefixer');
 let nested = require('postcss-nested');
 let assets = require('postcss-assets');
@@ -19,7 +18,8 @@ let b = (ext) => `./build/index.${ext}`;
     let bemjson = yamljs.load('index.yaml');
     let html = tmpl.apply(bemjson);
     postcss([
-      nested, pobems, assets,
+      nested,
+      assets,
       autoprefixer({browsers: ['> 1%', 'Last 2 versions']}),
       cssnano
     ]).process(fs.readFileSync(b('post.css'))).then(res => {
