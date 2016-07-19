081d203d3ba979384ff03a42b002ae7b36accc8f
Dmitry Starostin <incrop@gmail.com>
2016-07-20 00:46:44 +0300

    Improve jobs section

--- blocks/job/job.bemhtml.js
+++ blocks/job/job.bemhtml.js
@@ -1,9 +1,10 @@
 "use strict";
 
-block('job').content()((_, json) => [
+block('job').content()((ctx, json) => [
   {elem: 'header', content: [
+    ctx.mods.pic && {block: 'link', mix: {block: 'job', elem: 'pic'}, url: json.url},
     {elem: 'title', content: json.title},
-    {elem: 'company', content: json.company},
+    {block: 'link', mix: {block: 'job', elem: 'company'}, url: json.url, content: json.company},
     {elem: 'dates', content: json.dates},
   ]},
   applyNext()
