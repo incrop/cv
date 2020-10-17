4412522496036f64567ad3cd25dfcd4ec01246b3
Dmitry Starostin <incrop@gmail.com>
2020-10-17 19:52:41 +0300

    Updated CV

--- blocks/md/md.bemhtml.js
+++ blocks/md/md.bemhtml.js
@@ -7,10 +7,16 @@ block('md')(
     typeof line !== 'string'
       ? line
       : {html: (idx !== 0 && !line.startsWith(' ') ? ' ' : '') +
-          line.replace(/\[(.+?)\]\((.+?)\)/g, (_, text, url) => ctx.reapply({
+          line
+          .replace(/\[(.+?)\]\((.+?)\)/g, (_, text, url) => ctx.reapply({
             block: 'link',
             url: url,
             content: text
-          }))}
+          }))
+          .replace(/__(.+?)__/g, (_, text) => ctx.reapply({
+            tag: 'b',
+            content: text
+          }))
+        }
   ))
 );
